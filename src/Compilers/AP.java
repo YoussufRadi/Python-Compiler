06f5c88af37dd3fed6d36b4fdb9dcef89b35bbda
Youssuf Radi <y.sameh1995@gmail.com>
2018-04-26 12:39:36 +0200

    handle both codes

--- src/Compilers/AP.java
+++ src/Compilers/AP.java
@@ -5,36 +5,46 @@ import java_cup.runtime.Symbol;
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class AP {
 
-	public static void main(String [] args) {
+    static String inFile = System.getProperty("user.dir")+"/src/Compilers/in.in";
 
-		String inFile = System.getProperty("user.dir")+"/src/Compilers/in.in";
+    public static void main(String [] args) throws Exception {
+
 
 		if (args.length > 1) {
 			inFile = args[0];
 		}
 		
 		try {
-			FileInputStream fis = new FileInputStream(inFile);
-			BufferedInputStream bis = new BufferedInputStream(fis);
-			DataInputStream dis = new DataInputStream(bis);
-	
-			parser parser = new parser(new Lexer(dis));
-			Symbol res = parser.parse();
-			boolean value = ((Boolean)res.value).booleanValue();
+		    run();
+        } catch(FileNotFoundException e) {
+            inFile = System.getProperty("user.dir")+"/Python-Compiler/src/Compilers/in.in";
+            run();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-			if(value)
-				System.out.println("File: " + inFile + " parsed successfully.");
-			else
-				System.out.println("Error in parsing file: " + inFile);
-	
-			fis.close();
-			bis.close();
-			dis.close();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+    private static void run() throws Exception {
+
+        FileInputStream fis = new FileInputStream(inFile);
+        BufferedInputStream bis = new BufferedInputStream(fis);
+        DataInputStream dis = new DataInputStream(bis);
+
+        parser parser = new parser(new Lexer(dis));
+        Symbol res = parser.parse();
+        boolean value = ((Boolean)res.value).booleanValue();
+
+        if(value)
+            System.out.println("File: " + inFile + " parsed successfully.");
+        else
+            System.out.println("Error in parsing file: " + inFile);
+
+        fis.close();
+        bis.close();
+        dis.close();
+    }
 }
